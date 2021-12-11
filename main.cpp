93312fe91e8ffbf67a913f62cd70170f1bf30ef3
Samuele Pelloni <91503299+samuelepellonii@users.noreply.github.com>
2021-12-11 19:41:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
+
+
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int posizione;
+    char lista[10][20], nome[1][20];
+
+    for (int i=0; i<10; i++){
+        cin >> lista[i];
+    }
+    cin >> nome[0];
+
+    if (ricerca(lista, nome, posizione)){
+        cout << posizione << endl;
+    }else{
+        cout << "non presente" << endl;
+    }
+    return 0;
 }
