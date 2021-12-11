c16802cc1c99c5717c20ed72891422a2ed0c6603
Samuele Pelloni <91503299+samuelepellonii@users.noreply.github.com>
2021-12-11 19:41:08 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
 
+#include "lib.h"
+#include <cstring>
+
+bool ricerca (char lista [10][20], char nome[1][20], int &posizione){
+
+    for (int i=0; i<10; i++){
+        if (strcmp (lista[i], nome[0]) == 0){
+            posizione = i;
+            return true;
+        }
+    }
+    return false;
+
+}
